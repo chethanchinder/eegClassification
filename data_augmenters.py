fd586c6f748215c8c7e46409f487aeae5cf379c2
rakshithrgore <92978082+rakshithrgore@users.noreply.github.com>
2022-03-14 02:14:07 -0700

    Add files via upload

--- data_augmenters.py
+++ data_augmenters.py
@@ -29,13 +29,13 @@ class data_augmenter(object):
 
     # Data augmentation example which is subsample and add noise
 
-    def sub_sample_maxpool(self,X,y,sub_sample,average,noise):
+    def sub_sample_maxpool(self,X,y,sub_sample,average,noise,std=0.5,time_info=250,trim=500):
         
         total_X = None
         total_y = None
         
         # Trimming the data (sample,22,1000) -> (sample,22,500)
-        X = X[:,:,0:500]
+        X = X[:,:,0:trim]
         print('Shape of X after trimming:',X.shape)
         
         # Maxpooling the data (sample,22,1000) -> (sample,22,500/sub_sample)
@@ -48,7 +48,7 @@ class data_augmenter(object):
         
         # Averaging + noise 
         X_average = np.mean(X.reshape(X.shape[0], X.shape[1], -1, average),axis=3)
-        X_average = X_average + np.random.normal(0.0, 0.5, X_average.shape)
+        X_average = X_average + np.random.normal(0.0, std, X_average.shape)
         
         total_X = np.vstack((total_X, X_average))
         total_y = np.hstack((total_y, y))
@@ -59,14 +59,14 @@ class data_augmenter(object):
         for i in range(sub_sample):
             
             X_subsample = X[:, :, i::sub_sample] + \
-                                (np.random.normal(0.0, 0.5, X[:, :,i::sub_sample].shape) if noise else 0.0)
+                                (np.random.normal(0.0, std, X[:, :,i::sub_sample].shape) if noise else 0.0)
                 
             total_X = np.vstack((total_X, X_subsample))
             total_y = np.hstack((total_y, y))
             
         
         print('Shape of X after subsampling and concatenating:',total_X.shape)
-        return total_X,total_y
+        return total_X[:,:,:time_info],total_y
     # overall pipeline for  Frequency domain trimming and subsampling 
 # The input is of format Num_trials* channels * time bins
 
